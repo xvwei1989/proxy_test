825b1df0382becd91b6159fa86801e44a266071f
wei <xvwei1989@gmail.com>
2018-06-07 00:23:42 +0800

    readcount

--- get_stats.py
+++ get_stats.py
@@ -1,5 +1,6 @@
 #-*-encoding:utf8-*-
 from requests_html import HTMLSession,HTML
+
 import asyncio
 import requests
 
@@ -12,7 +13,17 @@ async def render_page(r):
         content = await r.page.content()
     html = HTML(url=r.url, html=content.encode('utf8'), default_encoding='utf8')    
     r.__dict__.update(html.__dict__)
-    return r.links
+    ret = []
+    ret1 = []
+    print (r.absolute_links)
+    ele = r.find('span.read-count')
+    print (ele)
+    for k in ele:
+        ret.append(k.text)
+    ele1 = r.find('a.chapter-item')
+    for k in ele1:
+        ret1.append(list(k.absolute_links)[0])
+    return ret,ret1
 
 def main():
     url = 'https://read.douban.com/column/7709247/'
@@ -22,6 +33,5 @@ def main():
     loop = asyncio.get_event_loop()
     ret = loop.run_until_complete(render_page(r.html))
     print (ret)
-
 if __name__ == '__main__':
-    main() 
+    main()
