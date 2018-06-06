26fa731354a45b7be141ab679aa8281b1e5d84e5
wei_xu <wei.xu@tongdun.cn>
2018-06-06 18:04:23 +0800

    fix bug

--- get_stats.py
+++ get_stats.py
@@ -9,19 +9,19 @@ async def render_page(r):
             await r.page.click('li.btn-link')
         except:
             break
-        await content = r.page.content()
+        content = await r.page.content()
     html = HTML(url=r.url, html=content.encode('utf8'), default_encoding='utf8')    
     r.__dict__.update(html.__dict__)
     return r.links
 
 def main():
-    url = ''
+    url = 'https://read.douban.com/column/7709247/'
     sess = HTMLSession()
     r = sess.get(url)
-    r.html.render(keep_page=on)
+    r.html.render(keep_page=True)
     loop = asyncio.get_event_loop()
     ret = loop.run_until_complete(render_page(r.html))
-    print ret
+    print (ret)
 
 if __name__ == '__main__':
     main() 
